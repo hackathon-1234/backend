d667845e30a8ccf6285d343e3a843a0423d91834
AirosUr <uroboros7279@gmail.com>
2023-05-20 15:31:02 +0300

    test version on file service

---
+++ src/main/java/com/hackathon2/file_service/repo/StoredFileRepository.java
@@ -0,0 +1,15 @@
+package com.hackathon2.file_service.repo;
+
+import com.hackathon2.file_service.domain.StoredFile;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface StoredFileRepository extends CrudRepository<StoredFile, Long> {
+	@Query("select id, name from files")
+	Iterable<StoredFile> findFilesList();
+
+}
---
+++ src/main/java/com/hackathon2/file_service/service/StoredFileServiceImpl.java
@@ -0,0 +1,104 @@
+package com.hackathon2.file_service.service;
+
+import com.hackathon2.file_service.domain.StoredFile;
+import com.hackathon2.file_service.dto.StoredFileDto;
+import com.hackathon2.file_service.repo.StoredFileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.io.FileNotFoundException;
+import java.util.Optional;
+
+@Service
+public class StoredFileServiceImpl implements StoredFileService {
+
+	private final StoredFileRepository storedFileRepository;
+
+	private static final String ERROR_MESSAGE = "File to update not found";
+	
+	@Autowired
+	public StoredFileServiceImpl(StoredFileRepository storedFileRepository) {
+		super();
+		this.storedFileRepository = storedFileRepository;
+	}
+
+	@Override
+	public Long create(StoredFileDto file) {
+		StoredFile f = StoredFile.builder()
+				.name(file.getName())
+				.build();
+		return storedFileRepository.save(f).getId();
+	}
+
+	@Override
+	public void update(Long id, StoredFileDto f) throws FileNotFoundException {
+		Optional<StoredFile> oFile = storedFileRepository.findById(id);
+		if (oFile.isEmpty()) {
+			throw new FileNotFoundException(String.format(ERROR_MESSAGE,id));
+		}
+		else {
+			StoredFile file = oFile.get();
+			file.setName(f.getName());
+			storedFileRepository.save(file);
+		}
+	}
+
+	@Override
+	public StoredFileDto get(Long id) throws FileNotFoundException {
+		Optional<StoredFile> oFile = storedFileRepository.findById(id);
+		if (oFile.isEmpty()) {
+			throw new FileNotFoundException(String.format(ERROR_MESSAGE,id));
+		}
+		else {
+			StoredFile f = oFile.get();
+			return StoredFileDto.builder()
+					.name(f.getName())
+					.build();
+		}
+	}
+
+	@Override
+	public String getName(Long id) throws FileNotFoundException {
+		Optional<StoredFile> oFile = storedFileRepository.findById(id);
+		if (oFile.isEmpty()) {
+			throw new FileNotFoundException(String.format(ERROR_MESSAGE,id));
+		}
+		else {
+			StoredFile f = oFile.get();
+			return f.getName();
+		}
+	}
+
+	@Override
+	public byte[] download(Long id) throws FileNotFoundException {
+		Optional<StoredFile> oFile = storedFileRepository.findById(id);
+		if (oFile.isEmpty()) {
+			throw new FileNotFoundException(String.format(ERROR_MESSAGE,id));
+		}
+		else {
+			return oFile.get().getContent();
+		}
+	}
+
+	@Override
+	public void upload(Long id, byte[] content) throws FileNotFoundException {
+		Optional<StoredFile> oFile = storedFileRepository.findById(id);
+		if (oFile.isEmpty()) {
+			throw new FileNotFoundException(String.format(ERROR_MESSAGE,id));
+		}
+		else {
+			StoredFile file = oFile.get();
+			file.setContent(content);
+			storedFileRepository.save(file);
+		}
+		
+	}
+
+	@Override
+	public void delete(Long id) {
+		StoredFile storedFile = storedFileRepository.findById(id)
+				.orElseThrow(() -> new EntityNotFoundException("Stored file with id# " + id + " not found"));
+		storedFileRepository.delete(storedFile);
+	}
+}
