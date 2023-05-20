05affc0a2b3751f8c3f354c9053c487c50f0234d
MakhachB <105069099+MakhachB@users.noreply.github.com>
2023-05-20 23:39:22 +0300

    create base repositories №2

--- src/main/java/com/hackathon2/auth/repository/QuestionRepository.java
+++ src/main/java/com/hackathon2/auth/repository/QuestionRepository.java
@@ -1,7 +1,6 @@
 package com.hackathon2.auth.repository;
 
 import com.hackathon2.model.Question;
-import liquibase.pro.packaged.R;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
