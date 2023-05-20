c804c6d34a75f74297086eb182b0de6de878ba1c
AirosUr <uroboros7279@gmail.com>
2023-05-20 15:10:34 +0300

    test token MD5 hash method

--- src/main/java/com/hackathon2/auth/utils/TokenUtils.java
+++ src/main/java/com/hackathon2/auth/utils/TokenUtils.java
@@ -6,6 +6,10 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.util.StringUtils;
 
+import javax.xml.bind.DatatypeConverter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class TokenUtils {
 
     private static final JwtDecoder jwtDecoder = new NimbusJwtDecoder(new ParseOnlyJWTProcessor());
@@ -20,5 +24,19 @@ public class TokenUtils {
         String token = StringUtils.delete(authHeader, "Bearer ");
         Jwt jwt = jwtDecoder.decode(token);
         return (String) jwt.getClaims().get("is_admin");
+    }
+
+    public static String hashOf(String s)  {
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        assert md != null;
+        md.update(s.getBytes());
+        byte[] digest = md.digest();
+        return DatatypeConverter
+                .printHexBinary(digest).toUpperCase();
     }
 }
