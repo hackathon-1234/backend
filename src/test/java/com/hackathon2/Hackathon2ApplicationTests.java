5e08e38ca322164524f00a2a71252681be7dc0e9
MakhachB <105069099+MakhachB@users.noreply.github.com>
2023-05-20 11:57:10 +0300

    initial commit

---
+++ src/test/java/com/hackathon2/Hackathon2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackathon2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
